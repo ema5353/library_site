d16a3de737cd13e1759c47a5c536aed90008abe1
ema5353 <emirkudai@icloud.com>
2023-03-02 23:17:47 +0600

    add models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Publisher, Book
+
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Book)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+
